d7406ba347738efbc9db0c40d0a6000b9f4a4847
Stephen20120619 <60807195+Stephen20120619@users.noreply.github.com>
2021-01-18 22:26:43 +0800

    mongoose initialize

--- loader/app.js
+++ loader/app.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const mongoose = require('mongoose');
+const Campground = require('../model/campground');
+const methodOverride = require('method-override');
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp', {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => {
+        console.log('Database connection open');
+    })
+    .catch(err => {
+        console.log('Cannot connect database');
+        console.log(err);
+    })
+
 
 app.listen(8080, () => {
 
@@ -8,9 +21,21 @@ app.listen(8080, () => {
 });
 
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname), '../views');
+app.set('views', path.join(__dirname, '../views'));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
 
-app.get('/', (req, res) =>{
+app.get('/', (req, res) => {
 
-    res.send('HELLO FORM YELP CAMP');
+    res.render('home/home');
+});
+
+app.post('/campground', (req, res) => {
+
+    const camp = new Campground(req.body);
+
+    camp.save();
+
+    res.send(camp);
 })
