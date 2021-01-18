1f4944d056c9c749dabc76a0e7ab9f903ea744fb
Stephen20120619 <60807195+Stephen20120619@users.noreply.github.com>
2021-01-18 23:50:12 +0800

    add PATCH route /campgrounds/:id --update single data from database
    add GET route /campgrounds/:id/edit
    --get edit page
    
    add POST route /campgrounds
    -- create data
    
    add GET route /camgrounds/new
    -- get create page

--- loader/app.js
+++ loader/app.js
@@ -39,6 +39,11 @@ app.get('/campgrounds', async (req, res) => {
     res.render('campgrounds/index', {campgrounds})
 });
 
+app.get('/campgrounds/new', (req, res) => {
+
+    res.render('campgrounds/new')
+});
+
 app.get('/campgrounds/:id', async (req, res) => {
 
     const {id} = req.params;
@@ -46,4 +51,32 @@ app.get('/campgrounds/:id', async (req, res) => {
     const campground = await Campground.findById(id)
 
     res.render('campgrounds/show', {campground})
+});
+
+app.get('/campgrounds/:id/edit', async (req, res) => {
+
+    const {id} = req.params;
+
+    const campground = await Campground.findById(id);
+
+    res.render('campgrounds/edit', {campground});
+});
+
+app.post('/campgrounds', async (req, res) => {
+
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+
+    res.redirect(`/campgrounds/${campground._id}`);
+
+});
+
+app.patch('/campgrounds/:id', async (req, res) => {
+
+    const {id} = req.params;
+    const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground}, {new: true});
+
+    res.redirect(`/campgrounds/${campground._id}`);
 })
+
+
